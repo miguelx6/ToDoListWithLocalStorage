9fd669736d57b78e2b7f0fe338cd9c6c7cd93faa
Miguel Cortes <mcortes21@ucol.mx>
2024-02-01 16:08:03 -0600

    Arreglo final

--- privada/index.php
+++ privada/index.php
@@ -1,11 +1,15 @@
 <?php
- //Se asegura que el usuario este autenticado
- require_once("login.php"); 
- $atributos = $saml->getAttributes(); //Obtiene sus atributos
- echo "Bienvenido ";
-  //Imprime los atributos
- foreach ($atributos as $clave => $valor) {
-    echo "<br><b>".$clave."</b>:".$valor[0];
+// Se asegura de que el usuario esté autenticado
+require_once("login.php");
+
+$atributos = $saml->getAttributes(); // Obtiene sus atributos
+echo "Bienvenido ";
+// Imprime los atributos
+foreach ($atributos as $clave => $valor) {
+    echo "<br><b>" . $clave . "</b>:" . $valor[0];
 }
- echo "<br><br>Usted se encuentra en la secci&oacute;n privada de esta aplicaci&oacute;n<br><a href='../'>Ir a secci&oacute;n p&uacute;blica</a><br><a href='logout.php'>[Cerrar sesi&oacute;n]</a>";
+echo "<br><br>Usted se encuentra en la sección privada de esta aplicación<br>";
+// Agrega el enlace para ir a la sección pública con la redirección a index.html
+echo "<a href='../index.html'>Ir a sección pública</a><br>";
+echo "<a href='logout.php'>[Cerrar sesión]</a>";
 ?>
